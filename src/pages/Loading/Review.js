3b61617721744ab2846cbd8fec2cdff38ed314ef
NishithHM <nhm987@gmail.com>
2023-10-03 12:39:39 +0530

    fixed

--- src/pages/Loading/Review.js
+++ src/pages/Loading/Review.js
@@ -114,8 +114,6 @@ const CustomerReview = () => {
   }
   return (
     <>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7389479959832649"
-     crossorigin="anonymous"></script>
       <ToastContainer
         position="top-center"
         autoClose={3000}
