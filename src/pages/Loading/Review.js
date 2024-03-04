6428813d117cac34d712c0af2fffca3eb0239a1d
NishithHM <nhm987@gmail.com>
2024-03-04 08:29:55 +0530

    build

--- src/pages/Loading/Review.js
+++ src/pages/Loading/Review.js
@@ -139,7 +139,7 @@ const CustomerReview = () => {
       setTimeout(() => {
         setRedirecting(false)
         window.open(url, '_blank')
-      }, 6000);
+      }, 4000);
       setTimeout(()=>{
         navigate('?session=expired')
       }, 1000)
